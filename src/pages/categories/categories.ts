39990e06b8c4ba2ec72f8e2ef530c20f056da486
janaite <janaite@gmail.com>
2021-05-28 15:03:42 -0300

    show categories

--- src/pages/categories/categories.ts
+++ src/pages/categories/categories.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
 import { CategoryService } from '../../services/domain/category.service';
+import { CategoryDTO } from '../../models/category.dto';
 
 /**
  * Generated class for the CategoriesPage page.
@@ -16,6 +18,10 @@ import { CategoryService } from '../../services/domain/category.service';
 })
 export class CategoriesPage {
 
+  bucketUrl: string = API_CONFIG.bucketBaseUrl;
+
+  items: CategoryDTO[];
+  
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
@@ -24,7 +30,7 @@ export class CategoriesPage {
 
   ionViewDidLoad() {
     this.categoryService.findAll().subscribe(response => {
-      console.log(response);
+      this.items = response;
     }, error => {
       console.log(error);
     });
